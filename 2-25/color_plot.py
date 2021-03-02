ba52ef1e6bc56ec074e8033205847b909e992562
a2saur <brownscholar@BridgeUP-Lap-311.home>
2021-03-02 17:12:49 -0500

    added changes

--- 2-25/color_plot.py
+++ 2-25/color_plot.py
@@ -4,30 +4,36 @@ from netCDF4 import Dataset
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
-w_file = 'path/to/w/file' # fix this
+# Open vertical velocity file
+w_file = './test/ss1a2ww.gr'
 w = open(w_file, "r")
 
 #open netcdf file
-original_data = 
+original_data = Dataset("/Users/brownscholar/Desktop/A2 Internship/n-atlantic-2018.nc")
 
 #get latitude and longitude from netcdf file: 
-# here:
-lat =
-lon = 
+lat = original_data.variables['latitude']
+lon = original_data.variables['longitude']
 
 num_lat = 158
 num_lon = 122
 levels = 1
 
-# make empty numpy array of shape lat, lon depth shape for storing w:
-
+# make empty numpy array of shape depth, lat, lon shape for storing w:
+empty_arr = np.zeros((levels, num_lat, num_lon))
 
 # use a loop to read w_file into the variable 
 #(skip first two lines of the file)
+w.readline()
+w.readline()
+for x in range(0,1):
+	for y in range(0,158):
+		for z in range(0,122):
+			line = w.readline()
+			empty_arr[x,y,z] = float(line)
 
 
-
-#this stuff defines the colorspace (we can google colormaps to learn more if we want to)
+#this stuff defines the colorspace
 top = cm.get_cmap('Blues_r', 128)
 bottom = cm.get_cmap('Reds', 128)
 
@@ -38,13 +44,20 @@ newcmp = ListedColormap(newcolors, name='RedBlue')
 
 
 
-#now use the following function to plot your data:
-#function to make colorplot is:
-# p = plt.pcolormesh(V,cmap = newcmp), where V is the numpy array with the data
+# plot the data:
+plt.pcolormesh(empty_arr[0,2:-2,2:-2], cmap=newcmp)
 
-# you can use these to add the x and y ticks to your plot:
-# I am happy to talk to you about why this works
-#plt.xticks(np.arange(0,num_lon,10),lon[::10])
-#plt.yticks(np.arange(0,num_lat,10),lat[::10])
+# add the x and y ticks to your plot:
+plt.xticks(np.arange(0,num_lon,10),lon[::10])
+plt.yticks(np.arange(0,num_lat,10),lat[::10])
 
 
+# add labels
+# labels_to_add = {"Upwelling":"#F00", "Downwelling":"#00F"}
+# for label in labels_to_add:
+# 	plt.plot([],[], color=labels_to_add[label], label=label)
+
+
+plt.colorbar()
+plt.legend()
+plt.show()
